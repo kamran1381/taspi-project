9672f28436572e8418e78c2de71cb147ca3a254e
kamran <ka6471381@gmail.com>
2022-07-19 17:04:02 +0430

    tapsi project commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//  let random=document.querySelectorAll(".p-inner");
+
+
+//  random.addEventListener("click",mouseOver);
+//  random.addEventListener("mouseout",mouseOut);
+
+// function mouseOver(){
+//    // random.classlist.add("p-inner");
+//    if(random.style.display=="none"){
+//          random.classlist.add("p-inner");
+//    }else{
+//       random.classlist.remove("p-inner");
+
+//    }
+// }
+
+// function mouseOut(){
+//    random.style.display="none";
+// }
